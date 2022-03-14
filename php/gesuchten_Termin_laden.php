6583cf95abdde69b2a275d736da623a785887b55
Cam Van Nguyen <van@M1P14.ip>
2022-03-14 21:29:49 +0100

    draft for search with filter

--- php/gesuchten_Termin_laden.php
+++ php/gesuchten_Termin_laden.php
@@ -1,31 +1,44 @@
 <?php
 $bedingungen_array = array();
 # Suche
-if(isset($_SESSION["suche"]) && $_SESSION["suche"] != "")
+$search_input = explode(" ", $_SESSION["suche"]);
+#if(isset($_SESSION["suche"]) && $_SESSION["suche"] != "")
+# Geb März unerl, Geb unerl, März unerl
+# Januar .... Dezember -> März => -03-
+# unerledigt, abgeschlossen .... -> starts with unerl
+# 10:00
+if(isset($search_input[0]) && $search_input[0] != "")
 {
+    #$_bedingung = $_SESSION["suche"];
+    $_bedingung = $search_input[0];
 	$bedingungen_array[] = "(
-	beschreibung LIKE '%".$_SESSION["suche"]."%'
-	OR
-	datum LIKE '%".$_SESSION["suche"]."%'
-	OR
-	zeit LIKE '%".$_SESSION["suche"]."%'
-    OR
-    s.bezeichnung LIKE '%".$_SESSION["suche"]."%'
-	)";
-}
-
-
-$bedingungen = "";
-if(count($bedingungen_array) > 0)
-{
-    $bedingungen = " WHERE ";
-	$bedingungen .= implode(" AND ", $bedingungen_array);
-}
-
-$sql_befehl = "
-select * from termine as t INNER JOIN statusmoeglichkeiten as s ON t.status_fk = s.status_pk " .$bedingungen. "order by datum";
-
-#var_dump($sql_befehl);
+        beschreibung LIKE '%".$_bedingung."%'
+        OR
+        datum LIKE '%".$_bedingung."%'
+        OR
+        zeit LIKE '%".$_bedingung."%'
+        )";
+        #s.bezeichnung LIKE '%".$_bedingung."%'
+    }
+    
+    if(isset($search_input[1]) && $search_input[1] != "") {
+        $_filter = $search_input[1];
+        
+        $bedingungen_array[] = "s.bezeichnung LIKE '%".$_filter."%'";
+    }
+    
+    
+    $bedingungen = "";
+    if(count($bedingungen_array) > 0)
+    {
+        $bedingungen = " WHERE ";
+        $bedingungen .= implode(" AND ", $bedingungen_array);
+    }
+    
+    $sql_befehl = "
+    select * from termine as t INNER JOIN statusmoeglichkeiten as s ON t.status_fk = s.status_pk " .$bedingungen. "order by datum";
+    
+//var_dump($sql_befehl);
 
 $antwort = mysqli_query($link, $sql_befehl);
 
--- php/termin_suche.php
+++ php/termin_suche.php
@@ -1,14 +1,21 @@
+<?php
+if (isset($_POST["suchbutton"])) {
+    $_SESSION["suche"] = $_POST["suche"];
+}
+?>
 <form method="POST">
     Suche: <input type="text" name="suche" value="<?= @$_SESSION["suche"]; ?>">
     <input type="submit" name="suchbutton" value="Suchen">
 </form>
 
 <?php
-if (isset($_POST["suchbutton"])) {
-    $_SESSION["suche"] = $_POST["suche"];
+if (isset($_SESSION["suche"]) && $_SESSION["suche"] != "") {
+    #var_dump($_SESSION["suche"]);
     include("gesuchten_Termin_laden.php");
 }
 ?>
+
+
 
 
 
